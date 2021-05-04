8941aefde8e10460c90ca9be9201a32d2b247f7b
Jeeva Kandasamy <jkandasa@gmail.com>
2021-05-04 13:07:45 +0530

    github workflow
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# update docsy
+git submodule update --init --recursive
+
+# install npm dependencies
+yarn install
+
+# build
+hugo -D
+
+# container registry
+REGISTRY='quay.io/mycontroller-org'
+IMAGE_WEBSITE="${REGISTRY}/website"
+IMAGE_TAG=`git rev-parse --abbrev-ref HEAD`
+
+# debug lines
+echo $PWD
+ls -alh
+git branch
+
+# build image
+docker buildx build \
+  --push \
+  --progress=plain \
+  --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 \
+  --file docker/Dockerfile \
+  --tag ${IMAGE_CORE}:${IMAGE_TAG} .
