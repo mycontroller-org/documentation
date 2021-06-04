15bd68657416a50b865292f99b28c3727990cfc8
Jeeva Kandasamy <jkandasa@gmail.com>
2021-06-04 12:17:40 +0530

    push to docker too
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,9 @@ yarn install
 env HUGO_ENV="production" hugo -D
 
 # container registry
-REGISTRY='quay.io/mycontroller-org'
-IMAGE_WEBSITE="${REGISTRY}/documentation"
+REGISTRY='quay.io/mycontroller'
+ALT_REGISTRY='docker.io/mycontroller'
+IMAGE_WEBSITE="documentation"
 IMAGE_TAG=`git rev-parse --abbrev-ref HEAD`
 
 # debug lines
@@ -20,10 +21,18 @@ echo $PWD
 ls -alh
 git branch
 
-# build image
+# build image and push to quay.io
 docker buildx build \
   --push \
   --progress=plain \
   --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 \
   --file docker/Dockerfile \
-  --tag ${IMAGE_WEBSITE}:${IMAGE_TAG} .
+  --tag ${REGISTRY}/${IMAGE_WEBSITE}:${IMAGE_TAG} .
+
+# build image and push to docker.io
+docker buildx build \
+  --push \
+  --progress=plain \
+  --platform linux/arm/v6,linux/arm/v7,linux/arm64,linux/amd64 \
+  --file docker/Dockerfile \
+  --tag ${ALT_REGISTRY}/${IMAGE_WEBSITE}:${IMAGE_TAG} .
